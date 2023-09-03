e740ad2730460902589411995aad75ad4313efe7
TimNguyen0606 <ChiThien.Nguyen-1@student.uts.edu.au>
2023-09-04 01:30:49 +0700

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,10 +7,12 @@ public class PrintAndHide : MonoBehaviour
     // Start is called before the first frame update
     int i;
     public Renderer rend;
+    int randomInt;
 
     void Start()
     {
         i = 3;
+        randomInt = Random.Range(150, 251);
     }
 
     // Update is called once per frame
@@ -18,5 +20,10 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        if (gameObject.CompareTag("Red") && i == 100)
+            gameObject.SetActive(false);
+        if (gameObject.CompareTag("Blue") && i == randomInt)
+            rend.enabled = false;
+
     }
 }
